80f4fc92026f9716afb6edebc4f16fff16ff9a6d
Gustavo Cremonez <aparecido2332001@gmail.com>
2023-08-17 11:28:06 -0300

    Add: Product entity and its unit test cases

---
+++ DotNETAssesmentGA.Domain/Entities/Product.cs
@@ -0,0 +1,40 @@
+﻿using DotNETAssesmentGA.Domain.Validation;
+
+namespace DotNETAssesmentGA.Domain.Entities
+{
+    public sealed class Product
+    {
+        public Product(string name, string description, decimal price)
+        {
+            this.ValidateEntity(name, description, price);
+
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public void Update(string name, string description, decimal price)
+        {
+            this.ValidateEntity(name, description, price);
+
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        private void ValidateEntity(string name, string description, decimal price)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description. Description is required");
+            DomainExceptionValidation.When(price < 0, "Invalid price value");
+        }
+    }
+}
---
+++ DotNETAssesmentGA.Tests/Domain/ProductUnitTest.cs
@@ -0,0 +1,126 @@
+﻿using DotNETAssesmentGA.Domain.Entities;
+using DotNETAssesmentGA.Domain.Validation;
+using FluentAssertions;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace DotNETAssesmentGA.Tests.Domain
+{
+    public class ProductUnitTest
+    {
+        [Fact(DisplayName = "CreateProduct_WithValidParameters_ResultObjectValidState")]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () =>
+            {
+                new Product(
+                    "Product Name",
+                    "Product Description",
+                    10.2m);
+            };
+
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "CreateProduct_WithNullOrEmptyNameValue_ThrowNameIsRequiredException")]
+        public void CreateProduct_WithNullOrEmptyNameValue_ThrowNameRequiredException()
+        {
+            Action actionEmpty = () =>
+            {
+                new Product(string.Empty, "Product Description", 10.2m);
+            };
+
+            actionEmpty.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name. Name is required");
+        }
+
+        [Fact(DisplayName = "CreateProduct_WithNullOrEmptyDescriptionValue_ThrowDescriptionRequiredException")]
+        public void CreateProduct_WithNullOrEmptyDescriptionValue_ThrowDescriptionRequiredException()
+        {
+            Action actionEmpty = () =>
+            {
+                new Product("Product Name", string.Empty, 10.2m);
+            };
+
+            actionEmpty.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid description. Description is required");
+        }
+
+        [Fact(DisplayName = "CreateProduct_WithInvalidPriceValue_ThrowInvalidPriceException")]
+        public void CreateProduct_WithInvalidPriceValue_ThrowInvalidPriceException()
+        {
+            Action action = () =>
+            {
+                new Product("Product Name", "Product Description", -10.2m);
+            };
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid price value");
+        }
+
+        [Theory(DisplayName = "UpdateProduct_WithValidParameters_ReturnUpdatedObject")]
+        [InlineData("New Name", "new Description", 1)]
+        public void UpdateProduct_WithValidParameters_ReturnUpdatedObject(string name, string description, decimal price)
+        {
+            Product product = new Product(
+                    "Product Name",
+                    "Product Description",
+                    10.2m);
+
+            Action action = () =>
+            {
+                product.Update(name, description, price);
+            };
+
+            action.Should().NotThrow<DomainExceptionValidation>();
+            action.Equals(product.Name == name);
+            action.Equals(product.Description == description);
+            action.Equals(product.Price == price);
+        }
+
+        [Fact(DisplayName = "UpdateProduct_WithNullOrEmptyNameValue_ThrowNameIsRequiredException")]
+        public void UpdateProduct_WithNullOrEmptyNameValue_ThrowNameRequiredException()
+        {
+            Product product = new Product(
+                    "Product Name",
+                    "Product Description",
+                    10.2m);
+
+            Action action = () =>
+            {
+                product.Update("", "new Description", 1);
+            };
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name. Name is required");
+        }
+
+        [Fact(DisplayName = "UpdateProduct_WithNullOrEmptyDescriptionValue_ThrowDescriptionRequiredException")]
+        public void UpdateProduct_WithNullOrEmptyDescriptionValue_ThrowDescriptionRequiredException()
+        {
+            Product product = new Product(
+                    "Product Name",
+                    "Product Description",
+                    10.2m);
+
+            Action action = () =>
+            {
+                product.Update("New Name", "", 1);
+            };
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid description. Description is required");
+        }
+
+        [Fact(DisplayName = "UpdateProduct_WithInvalidPriceValue_ThrowInvalidPriceException")]
+        public void UpdateProduct_WithInvalidPriceValue_ThrowInvalidPriceException()
+        {
+            Product product = new Product(
+                    "Product Name",
+                    "Product Description",
+                    10.2m);
+
+            Action action = () =>
+            {
+                product.Update("New Name", "new Description", -1);
+            };
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid price value");
+        }
+    }
+}
